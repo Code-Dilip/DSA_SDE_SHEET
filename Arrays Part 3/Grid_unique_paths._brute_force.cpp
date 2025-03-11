de576754fe9c42a92e10096f5e5af740b75531bf
Code_Dilip <learndilip05@gmail.com>
2025-03-11 21:57:24 +0530

    Grid unique paths brute force approach

---
+++ Arrays Part 3/Grid_unique_paths._brute_force.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int count_possible_paths(int i, int j, int n, int m){
+    if(i==n-1 && j==m-1){
+        return 1;
+    }
+    if(i>=n || j>=m){
+        return 0;
+    }
+    return count_possible_paths(i+1,j,n,m) + count_possible_paths(i,j+1,n,m);
+}
+
+int main(){
+    int n = 2;
+    int m = 3;
+    cout<<count_possible_paths(0,0,n,m)<<endl;
+    return 0;
+}
