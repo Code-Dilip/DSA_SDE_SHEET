eeb48a0b3e60b2b38c89540c36113f0c10e9263c
Code_Dilip <learndilip05@gmail.com>
2025-03-11 22:13:24 +0530

    Grid unique paths Better approach using Dynamic Programming

---
+++ Arrays Part 3/Grid_unique_paths_better_soln.cpp
@@ -0,0 +1,24 @@
+// using Dynamic Programming
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int count_unique_paths(int i,int j,int n,int m,vector<vector<int>> &dp){
+    if(i==n-1 && j==m-1){
+        return 1;
+    }
+    if(i>=n || j>=m){
+        return 0;
+    }
+    if(dp[i][j]!=-1){
+        return dp[i][j];
+    }
+    else return count_unique_paths(i+1,j,n,m,dp) + count_unique_paths(i,j+1,n,m,dp);
+}
+
+int main(){
+    int n = 2, m = 3;
+    vector <vector<int>> dp(n,vector<int> (m,-1));
+    cout<<count_unique_paths(0,0,n,m,dp)<<endl;
+    return 0;
+}
