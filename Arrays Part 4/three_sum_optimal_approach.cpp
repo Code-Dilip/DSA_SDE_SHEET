1c15698d29306d6c302d91c68e0c87e8e808e15d
Code_Dilip <learndilip05@gmail.com>
2025-03-13 21:56:38 +0530

    three sum optimal solution

--- Arrays Part 4/three_sum_optimal_approach.cpp
+++ Arrays Part 4/three_sum_optimal_approach.cpp
@@ -6,23 +6,33 @@ using namespace std;
 
 vector <vector<int>> three_sum(vector <int> nums, int target){
     vector <vector<int>> ans;
-    int n = nums.size();
     sort(nums.begin(),nums.end());
+    int n = nums.size();
 
     for(int i=0;i<n;i++){
         if(i>0 && nums[i]==nums[i-1]){
             continue;
         }
+        set <int> hash_set;
         int j = i+1;
         int k = n-1;
-        while(j<k){
+        while (j<k)
+        {
             int sum = nums[i]+nums[j]+nums[k];
-            if(sum == target){
-                vector<int> temp = {nums[i],nums[j],nums[k]};
+            if(sum < target){
+                j ++;
+            }
+            else if(sum > target){
+                k --;
+            }
+            else{
+                vector <int> temp = {nums[i],nums[j],nums[k]};
+                sort(temp.begin(), temp.end());
                 ans.push_back(temp);
                 j ++;
                 k --;
-                while(j<k && nums[j]==nums[j-1]){
+                while (j<k && nums[j]==nums[j-1])
+                {
                     continue;
                 }
                 while (j<k && nums[k]==nums[k+1])
@@ -30,15 +40,8 @@ vector <vector<int>> three_sum(vector <int> nums, int target){
                     continue;
                 }
             }
-            else if(sum < target){
-                j ++;
-            }
-            else{
-                k--;
-            }
         }
     }
-
     return ans;
 }
 
